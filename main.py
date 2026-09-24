7c324122eb3317d4cb03293f67791c243f1f1fdd
Md Tariqul Islam <tariqulislam9392@gmail.com>
2026-09-24 21:42:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(title='Emergency Ambulance Dispatch System')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://stalwart-crumble-5d348c.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
